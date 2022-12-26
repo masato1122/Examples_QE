a60f6d30c2f13ba076ea613ee7cf46953153e934
masato1122 <masato1122@hotmail.co.jp>
2022-12-27 01:53:14 +0900

    update

---
+++ examples/1_relax/fit.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+import numpy as np
+from optparse import OptionParser
+
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+from mytool.mpl.initialize import (set_matplot, set_axis, set_legend)
+
+def plot_data(xdat, ydat, figname='fig_alat-E.png', 
+        dpi=300, fontsize=7, fig_width=2.3, aspect=0.9, lw=0.5, ms=2.0):
+    
+    ### fitting
+    out = np.polyfit(xdat, ydat, 2)
+    xc = -out[1] / 2./out[0]
+    print(" xcenter [Bohr]: ", xc)
+    
+    ###
+    set_matplot(fontsize=fontsize)
+    fig = plt.figure(figsize=(fig_width, aspect*fig_width))
+        
+    ax = plt.subplot()
+    ax.set_xlabel('${\\rm a_{lat} (Bohr)}$')
+    ax.set_ylabel('Energy (Ry)')
+    
+    ax.plot(xdat, ydat, linestyle='None', lw=lw, marker='o', markersize=ms,
+            mfc='none', mew=lw)
+    
+    ### fitting curve
+    nfit = 101
+    xfit = np.linspace(np.min(xdat), np.max(xdat), nfit)
+    yfit = np.polyval(out, xfit)
+    ax.plot(xfit, yfit, linestyle='-', c='black', lw=lw)
+
+    ax.axvline(xc, lw=0.5, linestyle='-', c='grey')
+     
+    set_axis(ax)
+    
+    fig.savefig(figname, dpi=dpi, bbox_inches='tight')
+    print(" Output", figname)
+    return fig
+
+
+def main(options):
+
+    data = np.genfromtxt(options.filename)
+    xdat = data[:,1]
+    ydat = data[:,2]
+    
+    plot_data(xdat, ydat)
+    
+if __name__ == '__main__':
+    parser = OptionParser()
+    parser.add_option("-f", "--filename", dest="filename", type="string",
+            default="energy.txt", help="input file name")
+    (options, args) = parser.parse_args()
+    #file_check(options.filename)
+    main(options)
--- examples/phonon_dfpt/README.md
+++ examples/phonon_dfpt/README.md
@@ -7,16 +7,29 @@ You can learn how to calculate phonon dispersion based on a density functional p
 
 # Contents
 
-* ``scripts`` : input scripts for QE
+* ``scripts`` : input scripts for simulations
 
-* ``results`` : final results
+* ``results`` : results: figures, output files, ...
 
 * ``shell`` : shell scripts to run every calculations
 
 # Calcution process
 
-## 1. 
+## 0. Copy input scripts
+
+```
+$ cp ./scripts/* ./
+```
+
+## 1. Run an SCF calculation
+
+First of all, run the SCF calculation same as the calculation of electron band structure.
+
+```
+$ pw.x < scf.in | tee scf.out
+``` 
+
+## 2. Calcualte eigenvalues at $\Gamma$ point
 
 
-##
 
